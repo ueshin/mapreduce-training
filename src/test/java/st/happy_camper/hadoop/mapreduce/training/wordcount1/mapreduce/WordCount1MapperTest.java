63b6ca95734db1aa6e8d97ae1136260964c3dfe5
Takuya UESHIN <ueshin@happy-camper.st>
2011-07-11 22:42:47 +0900

    MRUnitによるテストコードを追加。
    Signed-off-by: Takuya UESHIN <ueshin@happy-camper.st>

---
+++ src/test/java/st/happy_camper/hadoop/mapreduce/training/wordcount1/mapreduce/WordCount1MapperTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2011 Happy-Camper Street.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package st.happy_camper.hadoop.mapreduce.training.wordcount1.mapreduce;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author ueshin
+ */
+public class WordCount1MapperTest extends WordCount1Mapper {
+
+    private MapDriver<LongWritable, Text, Text, LongWritable> driver;
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        driver = new MapDriver<LongWritable, Text, Text, LongWritable>(new WordCount1Mapper());
+    }
+
+    /**
+     * Test method for
+     * {@link st.happy_camper.hadoop.mapreduce.training.wordcount1.mapreduce.WordCount1Mapper#map(org.apache.hadoop.io.LongWritable, org.apache.hadoop.io.Text, org.apache.hadoop.mapreduce.Mapper.Context)}
+     * .
+     */
+    @Test
+    public void testMapLongWritableTextContext() {
+        driver.withInput(new LongWritable(0L), new Text("Hello World"))
+                .withOutput(new Text("Hello"), new LongWritable(1L))
+                .withOutput(new Text("World"), new LongWritable(1L)).runTest();
+    }
+
+}
---
+++ src/test/java/st/happy_camper/hadoop/mapreduce/training/wordcount1/mapreduce/WordCount1ReducerTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2011 Happy-Camper Street.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package st.happy_camper.hadoop.mapreduce.training.wordcount1.mapreduce;
+
+import java.util.Arrays;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author ueshin
+ */
+public class WordCount1ReducerTest extends WordCount1Reducer {
+
+    private ReduceDriver<Text, LongWritable, Text, LongWritable> driver;
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        driver = new ReduceDriver<Text, LongWritable, Text, LongWritable>(new WordCount1Reducer());
+    }
+
+    /**
+     * Test method for
+     * {@link st.happy_camper.hadoop.mapreduce.training.wordcount1.mapreduce.WordCount1Reducer#reduce(org.apache.hadoop.io.Text, java.lang.Iterable, org.apache.hadoop.mapreduce.Reducer.Context)}
+     * .
+     */
+    @Test
+    public void testReduceTextIterableOfLongWritableContext() {
+        driver.withInput(new Text("Word"), Arrays.asList(new LongWritable(1L), new LongWritable(1L)))
+                .withOutput(new Text("Word"), new LongWritable(2L)).runTest();
+    }
+
+}
